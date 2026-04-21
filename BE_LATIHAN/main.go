75d5a7808854f0830f917ab11ba9f1e84db0feeb
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2026-04-21 11:52:43 +0700

    pertemuan6

--- BE_LATIHAN/main.go
+++ BE_LATIHAN/main.go
@@ -4,13 +4,21 @@ import (
 	"be_latihan/config"
 	"be_latihan/model"
 	"be_latihan/router"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: strings.Join(config.GetAllowedOrigins(), ","),
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+	}))
+
 	app.Use(logger.New())
 
 	config.InitDB()
